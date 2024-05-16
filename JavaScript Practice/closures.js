72cf51aa2cf18189218ea66e716d7b4c36245e5a
Shailendrajain08 <71479782+Shailendrajain08@users.noreply.github.com>
2024-05-16 19:27:15 +0000

    closeure with var

--- JavaScript Practice/closures.js
+++ JavaScript Practice/closures.js
@@ -14,19 +14,32 @@
 
 // closure with var 
 
-function x() {
-    for(var i = 1; i <= 5; i++) {
-        setTimeout(function () {
-            console.log(i)
-        }, i * 1000)
-    }
-}x();
+// function x() {
+//     for(var i = 1; i <= 5; i++) {
+//         setTimeout(function () {
+//             console.log(i)
+//         }, i * 1000)
+//     }
+// }x();
 
 // closure with let 
-function y() {
-    for(let i = 1; i <= 5; i++) {
-        setTimeout(function () {
-            console.log(i)
-        }, i * 1000)
+// function y() {
+//     for(let i = 1; i <= 5; i++) {
+//         setTimeout(function () {
+//             console.log(i)
+//         }, i * 1000)
+//     }
+// }y();
+
+
+// for proper output with var 
+function x() {
+    for(var i = 1; i <= 5; i++) {
+        function close(i){
+            setTimeout(function () {
+                console.log(i)
+            }, i * 1000)
+        }
+        close(i)
     }
-}y();
+}x();
