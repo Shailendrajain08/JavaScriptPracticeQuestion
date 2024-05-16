e8be43158b90dc99e926b9a5e902bec37d4d4b4c
Shailendrajain08 <71479782+Shailendrajain08@users.noreply.github.com>
2024-05-16 19:11:38 +0000

    closure with let and var

--- JavaScript Practice/closures.js
+++ JavaScript Practice/closures.js
@@ -1,13 +1,32 @@
-function closure () {
-    let a = 2;
-     function closure2 () {
-        console.log(a);
-        let b = 8
-         function closure3() {
-            console.log(b);   
-        }
-        return closure3()
+// function closure () {
+//     let a = 2;
+//      function closure2 () {
+//         console.log(a);
+//         let b = 8
+//          function closure3() {
+//             console.log(b);   
+//         }
+//         return closure3()
+//     }
+//     return closure2()
+// }
+// closure()
+
+// closure with var 
+
+function x() {
+    for(var i = 1; i <= 5; i++) {
+        setTimeout(function () {
+            console.log(i)
+        }, i * 1000)
     }
-    return closure2()
-}
-closure()
+}x();
+
+// closure with let 
+function y() {
+    for(let i = 1; i <= 5; i++) {
+        setTimeout(function () {
+            console.log(i)
+        }, i * 1000)
+    }
+}y();
