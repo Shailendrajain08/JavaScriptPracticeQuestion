f20ff41499846ab05d43ce7e92ead40b7b3a7ee2
Shailendrajain08 <71479782+Shailendrajain08@users.noreply.github.com>
2024-05-29 14:52:37 +0000

    For loop with var and setTimeout

---
+++ JavaScript Practice/forLoopWithVarandlet.js
@@ -0,0 +1,26 @@
+// for(let i=0; i<10;i++) {
+//     setTimeout(()=> {
+//         console.log(i)
+//     })
+//     // console.log("2",i)
+// }
+
+// console.log("i",i)
+// var algo 
+
+// var i = 0;
+
+// if(i<10){
+//     setTimeout(()=>{
+//         console.log(i);
+//     })
+// }
+
+// i++ 
+
+// if(i<10){
+//     setTimeout(()=>{
+//         console.log(i);
+//     })
+// }
+
